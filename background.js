82535b4ab5f00622b98f94cee8d96a3720cab7a5
Vrinda Venugopal <vrindavenugopal2005@gmail.com>
2026-06-05 22:43:02 +0530

    Add action on click of clean copy

--- background.js
+++ background.js
@@ -1,7 +1,16 @@
+importScripts('utils/cleaner.js')
+
 chrome.runtime.onInstalled.addListener(()=>{
     chrome.contextMenus.create({
         "title" : "Copy Clean",
         "id" : "1",
         "contexts" : ["selection"]
     })
+})
+
+chrome.contextMenus.onClicked.addListener((info,tab)=>{
+    if(info.menuItemId=="1")
+    {
+        const text=info.selectionText
+    }
 })
