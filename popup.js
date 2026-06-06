18c3ba64a8f90167471fd45f3659e1f3275cce74
Vrinda Venugopal <vrindavenugopal2005@gmail.com>
2026-06-06 20:45:57 +0530

    Style popup and fix saved message

--- popup.js
+++ popup.js
@@ -3,6 +3,8 @@ const button=document.getElementById("saveBtn");
 
 button.addEventListener("click",()=>{
     chrome.storage.local.set({geminiKey:inputField.value})
+    const savedMsg=document.getElementById("savedMsg");
+    savedMsg.style.display="block"
 })
 
 chrome.storage.local.get("geminiKey",(result)=>{
